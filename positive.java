b316a2578c57a2492711f43c328a55f5d49b2f6e
adarshkapoor <adarshkapoor96@gmail.com>
2017-07-09 15:41:29 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+         
+       Scanner s=new Scanner(System.in);
+       
+       System.out.print("Enter a number:");
+       int a=s.nextInt(); 
+           
+        { if(a>0)
+           System.out.println("the number is positive");
+            else if(a<0)
+           System.out.println("the number is negetive");
+             else 
+            System.out.println("the number is zero");
+           }
+             }
+                }
